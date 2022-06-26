486ad34916c67e00de134d0f744aca7b33e1ce63
郑泳健 <yongjian.zheng@tongdun.net>
2022-06-26 17:36:32 +0800

    require改为import

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,8 +3,14 @@
  * @Author: 郑泳健
  * @Date: 2022-06-01 13:56:18
  * @LastEditors: 郑泳健
- * @LastEditTime: 2022-06-19 18:58:45
+ * @LastEditTime: 2022-06-26 17:23:30
  */
+require('ts-node').register({
+    compilerOptions: {
+        module: 'commonjs'
+    }
+});
+const shell = require('shelljs');
 const fs = require('fs');
 const path = require('path');
 const {
@@ -13,34 +19,50 @@ const {
     flatObject,
     getAdjustLangObjAndAddList,
     generateExcel,
-    rewriteFiles
+    rewriteFiles,
+    changeFileSuffix,
+    getNeedChangeNameFileList
 } = require('../utils/translate');
 
 // 同步不同的语言包
 function translate() {
-    const zhCN = require(otpPath + '/zh-CN');
-    const zhCNflat = flatObject(zhCN);
+    (async () => {
+        await shell.rm('-rf', path.resolve(__dirname, './temp'));
+        await shell.cp(
+            '-R',
+            otpPath,
+            path.resolve(__dirname, './temp')
+        );
+        const filelist = getNeedChangeNameFileList(path.resolve(__dirname, './temp'), '.js')
 
-    const otpConfig = fs.readFileSync(path.resolve(otpPath + '/../otp-config.json'), 'utf-8')
-    const distLang = otpConfig && JSON.parse(otpConfig) && JSON.parse(otpConfig).distLangs
+        changeFileSuffix(filelist, '.js', '.ts')
 
-    if (!Array.isArray(distLang)) {
-        console.log('请配置otp-config.json里面的distLangs')
-        return;
-    }
-    syncFiles(distLang);
+        const { default: zhCN } = require('./temp/zh-CN');
 
-    distLang.forEach((lang) => {
-        const langFlat = flatObject(require(otpPath + '/' + lang));
+        const zhCNflat = flatObject(zhCN);
 
-        // 删除掉多余的key，增加新的key，同时提取没有翻译过的key的列表
-        const { fileKeyValueList, addList } = getAdjustLangObjAndAddList(langFlat, zhCNflat);
+        const otpConfig = fs.readFileSync(path.resolve(otpPath + '/../otp-config.json'), 'utf-8')
+        const distLang = otpConfig && JSON.parse(otpConfig) && JSON.parse(otpConfig).distLangs
 
-        // 重写文件
-        rewriteFiles(fileKeyValueList, lang);
-        // 生成excel
-        generateExcel(addList, otpPath + '/' + lang, lang);
-    });
+        if (!Array.isArray(distLang)) {
+            console.log('请配置otp-config.json里面的distLangs')
+            return;
+        }
+
+        await syncFiles(distLang);
+
+        distLang.forEach((lang) => {
+            const langFlat = flatObject(require(`./temp/${lang}`));
+
+            // 删除掉多余的key，增加新的key，同时提取没有翻译过的key的列表
+            const { fileKeyValueList, addList } = getAdjustLangObjAndAddList(langFlat, zhCNflat);
+
+            // 重写文件
+            rewriteFiles(fileKeyValueList, lang);
+            // 生成excel
+            generateExcel(addList, otpPath + '/' + lang, lang);
+        });
+    })()
 }
 
 module.exports = {
